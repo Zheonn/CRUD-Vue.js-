02868dbf4a1cacc1faa2bdace39b07769c9d0200
Leonnardo Trindade <leonnardobtrindade@hotmail.com>
2022-12-05 14:53:38 -0300

    Project Complete

--- vue-crud/src/pages/admin/Admin.vue
+++ vue-crud/src/pages/admin/Admin.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="div-adm">
         <Nav></Nav>
         <div class="left-content">
             <SideBar></SideBar>
@@ -21,5 +21,5 @@ export default {
 </script>
 
 <style>
-
+    
 </style>
